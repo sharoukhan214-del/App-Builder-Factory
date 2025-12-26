c1944289e50fe87b2779fd52225e51c98bb03d6e
sharoukhan214-del <sharoukhan214@gmail.com>
2025-12-26 18:39:25 +0200

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: Scaffold(
+      body: Center(
+        child: Text(
+          'تم تشغيل المصنع بنجاح يا محمد!',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    ),
+  ));
+}
