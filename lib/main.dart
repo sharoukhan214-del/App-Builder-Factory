e5618d39bb7e45405666937dbeea214e9f1203c9
sharoukhan214-del <sharoukhan214@gmail.com>
2025-12-26 19:42:07 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,7 @@ void main() {
   runApp(const MaterialApp(
     home: Scaffold(
       body: Center(
-        child: Text(
-          'تم تشغيل المصنع بنجاح يا محمد!',
-          style: TextStyle(fontSize: 24),
-        ),
+        child: Text('تم تشغيل المصنع بنجاح يا محمد!'),
       ),
     ),
   ));
